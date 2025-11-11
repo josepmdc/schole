eba9fbf4493bd94a915cb1cf2fe7f5d9dbc44e68
josepmdc <jdomingocatafal@gmail.com>
2025-11-11 16:44:23 +0100

    deploy config

--- api/schole/settings.py
+++ api/schole/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-wje&_vo^=3l$0^+1lznni4=%trhf!!oufr@)(lsz0_0n&h(-^x"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["josepmdc.pythonanywhere.com"]
 
 
 # Application definition
